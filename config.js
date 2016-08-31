020de17bdf96ccd30ea7a8adafc7beeb5f2790a7
phil romanov <romanovexports@gmail.com>
2016-08-31 20:05:55 +0300

    add config

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	hosting: {
+		host: '185.18.54.156',
+		user: 'root',
+		pass: 'SJ4pp7YKYU'
+	},
+	gmail: {
+		user: 'yourselfrbot@gmail.com',
+		pass: '9b317a357323f9639c1c20437d951603',
+		namecheap: '#9b317A357323f9639c1c20437d951603'
+	},
+	vkToken: {
+		clientId: '5023767',
+		clientSecret: '6mZAz0f8JfYiZqEzZ9Y'
+	},
+	twitterToken: {
+		consumer: 'F641XWO78AKpQ0leASDHIwOEU',
+		apiSecret: 'qEBfFJGbF2JqjlvlPx5k2XhD8DWKuvbYuqyWf968zjdCpnsFVk',
+		ownerId: '729339351767887872'
+	},
+	port: 80
+}
